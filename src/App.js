61ac66da21734fbd4b690b48069c1b6c858cdacc
Adrián Domínguez Torcal <126667352+TorkiTorki@users.noreply.github.com>
2023-03-01 11:32:24 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Formulario from './componentes/formulario';
+
+function App() {
+  return(
+    <div className='formul'>
+      <Formulario/>
+    </div>
+    
+  )
+    
+}
+
+export default App;
