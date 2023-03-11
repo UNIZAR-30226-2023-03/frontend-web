2c5d53e717da4997fa1c638ca9117fd3e5630293
TorkiTorki <819478@unizar.es>
2023-03-11 12:30:29 +0100

    prueba axios

--- src/componentes/Login.Form.jsx
+++ src/componentes/Login.Form.jsx
@@ -2,27 +2,27 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 function LoginForm() {
-  const [login, setUsername] = useState('');
+  const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
 
   const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
+    setLogin(event.target.value);
   };
 
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     console.log(login, password);
     
-    fetch('https://backend-sy93.onrender.com/usuarios/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    })
+    // fetch('https://backend-sy93.onrender.com/usuarios/login', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   }
+    // })
     // fetch('/usuarios/login', {
     //   method: 'POST',
     //   body: JSON.stringify({login, password}),
@@ -42,14 +42,12 @@ function LoginForm() {
     //  })
     // .catch(error => console.error('Error',error))
     //.then(response => console.log(response))
-    // try{
-    //   const response = await axios.post("/api/login", {
-    //     username: username,
-    //     password: password,
-    //   });
-    // }catch(error){
-    //   console.error(error);
-    // }
+    try{
+      const response = await axios.post("https://backend-sy93.onrender.com/usuarios/login", {login, password});
+      console.log(response.data);
+    }catch(error){
+      console.error(error);
+    }
     // Validar la entrada del usuario aquí
 
     // Enviar datos al servidor aquí
@@ -59,12 +57,12 @@ function LoginForm() {
     <form onSubmit={handleSubmit}>
       <label>
         Usuario:
-        <input type="text" name="login" value={login} onChange={handleUsernameChange} />
+        <input type="text" value={login} onChange={handleUsernameChange} />
       </label>
       <br/>
       <label>
         Contraseña:
-        <input type="password" name="password" value={password} onChange={handlePasswordChange} />
+        <input type="password" value={password} onChange={handlePasswordChange} />
       </label>
       <br/>
       <button type="submit">Iniciar sesión</button>
