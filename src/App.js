3682a284f1bb1e1745dd38444ca82c4f2d4c8e9c
DavidArruga <816058@unizar.es>
2023-03-11 17:49:28 +0100

    Co-authored-by: adrian dominguez torcal <819478@unizar.es>

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return(
     <BrowserRouter>
     <Routes>
-        <Route  path='/' component={LoginForm}>
+        <Route  path='/' Component={LoginForm}>
         </Route>
     </Routes>
     </BrowserRouter>
