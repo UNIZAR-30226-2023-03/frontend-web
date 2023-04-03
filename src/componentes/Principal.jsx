2831fb4179f5bdd6e4d64f4ccab08c8a098ec081
DavidArruga <816058@unizar.es>
2023-04-03 12:02:18 +0200

    Movimiento de fichas

--- src/componentes/Principal.jsx
+++ src/componentes/Principal.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import "../styles/Principal.css";
-//import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import Cookies from 'universal-cookie';
 
