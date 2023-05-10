a8a5b09df2c91292e319102146d73f006aec361c
adrian <819478@unizar.es>
2023-05-10 18:58:54 +0200

    Tienda

--- src/componentes/Tienda.jsx
+++ src/componentes/Tienda.jsx
@@ -1,35 +1,89 @@
 import "../styles/Tienda.css";
 import home from "../imagenes/iconos/home.svg"
-//import axios from "axios";
-import React, {} from "react";
+import axios from "axios";
+import React, {useEffect, useState} from "react";
+import Cookies from 'universal-cookie';
 
 
 function Tienda(){
+  const cookies= new Cookies();
+  const idUsuario = cookies.get('idUsuario');
+  const [objetos, setTiend] = useState([]);
+  const obtenidos = Array(4).fill(false);
+  const [monedas, setmonedas] = useState(0);
+  useEffect(() => {
+    async function buscarmonedas() {
+      await axios.get("https://lamesa-backend.azurewebsites.net/usuario/monedas/"+idUsuario)
+      .then ( response => {
+        setmonedas(response.data);
+      })
+    }
+    buscarmonedas(); 
+    // eslint-disable-next-line
+  }, []);
+  useEffect(()=>{
+    async function consultarTienda(yo){
+      const response = await axios.get("https://lamesa-backend.azurewebsites.net/usuario/productos/"+yo);  
+      const objetos = response.data;
+      console.log("objetos",response.data);
+      const tiend = [];
+        objetos.forEach(objeto => {
+          tiend.push(objeto); 
+        });
+        setTiend(tiend);
+    }
+    consultarTienda(idUsuario)
+  }, [idUsuario])
 
-  //const [cosas, setcosas] = useState([]);
 
-
-  
-  // useEffect(() => {
-  //   async function obtenerTienda() {
-  //     await axios.get("https://lamesa-backend.azurewebsites.net/tienda")
-  //     const torneosactivos = [];
-  //     torneos.forEach(torneo => {
-  //       if (torneo.estado ==="ESPERANDO_JUGADORES") {
-  //         torneosactivos.push(torneo);
-  //         console.log("torneo : "+torneo.nombre);
-  //       }
-  //     });
-  //     settorneosActivos(torneosactivos);
-
-      
-  //   }
-  //   obtenerTienda(); 
-  // }, []);
-
+  useEffect(()=>{
+    async function recorrerVector(todos){
+      todos.forEach(objet=>{
+        if(objet==="1"){
+          obtenidos[0]=true;
+        }
+        else if (objet==="2"){
+          obtenidos[1]=true;
+        }
+        else if (objet==="3"){
+          obtenidos[2]=true;
+        }
+        else if (objet==="4"){
+          obtenidos[3]=true;
+        }
+        
+      });
+    }
+    recorrerVector(objetos)
+  }, [objetos,obtenidos])
+  async function id0() {
+    const response =await axios.post("https://lamesa-backend.azurewebsites.net/tienda/comprar", {usuario: idUsuario,producto:"1"});
+    console.log(response.data)
+    console.log("comprado tablero1");
+    window.location.reload(); 
+  }
+  async function id1() {
+    const response =await axios.post("https://lamesa-backend.azurewebsites.net/tienda/comprar", {usuario: idUsuario,producto:"2"});
+    console.log(response.data)
+    console.log("comprado tablero2");
+    window.location.reload(); 
+  }
+  async function id2() {
+    const response =await axios.post("https://lamesa-backend.azurewebsites.net/tienda/comprar", {usuario: idUsuario,producto:"3"});
+    console.log(response.data)
+    console.log("comprado ficha1");
+    window.location.reload(); 
+  }
+  async function id3() {
+    const response =await axios.post("https://lamesa-backend.azurewebsites.net/tienda/comprar", {usuario: idUsuario,producto:"4"});
+    console.log(response.data)
+    console.log("comprado ficha2");
+    window.location.reload();  
+  }
     return(
       
       <div>
+          <div class="monedasJugador1" data-number={monedas}></div>
           <div className="back7">
               <div className="breadcrumb">
               <div className="breadcrumb-item"><a href="principal"><img className="casa" src={home} alt="" /></a></div>
@@ -37,25 +91,26 @@ function Tienda(){
               <div className="breadcrumb-item">Tienda</div>
               </div>
           </div>
+          <div className="txt">
+            <p>Seleccione el producto que desee comprar</p> 
+          </div>
           <div className="compra">
-            <div className="container">
-              <button className="botn1">
-                Tablero de Navidad
-              </button>
+            <div className="bb">
+              <button className={obtenidos[0] ? "botn-no": "botn"} onClick={id0}></button>
+              <p className="asd">Despripcion del producto</p>
+            </div>
+            <div className="bb">
+              <button className={obtenidos[1] ? "botn-no": "botn"}  onClick={id1}></button>
+              <p className="asd">Despripcion del producto</p>
+            </div>
+            <div className="bb">
+              <button className={obtenidos[2] ? "botn-no": "botn"}  onClick={id2}></button>
+              <p className="asd">Despripcion del producto</p>
               </div>
-              <button className="botn">
-                Tablero de Halloween
-              </button>
-              <button className="botn">
-                Ficha de Navidad
-              </button>
-              <button className="botn">
-                Ficha de Halloween
-              </button>
-
-        
-
-
+            <div className="bb">
+              <button className={obtenidos[3] ? "botn-no": "botn"}  onClick={id3}></button>
+              <p className="asd">Despripcion del producto</p>
+              </div>
           </div>
       </div>
     );
